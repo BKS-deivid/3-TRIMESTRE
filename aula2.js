a84d625eb3501b12cbd581a92fe420c06dd7e89b
Maryana <maryana.azarias@escola.pr.gov.br>
2024-10-04 09:54:57 -0300

    java

---
+++ aula2.js
@@ -0,0 +1,11 @@
+let a = 60;
+let b = 20;
+let c = 2;
+
+let soma = a+b;
+
+if(soma > c){
+    console.log("O valor  "+ soma +"que o valor"+ c);
+}else{
+    console.log("O valor "+ soma + " é menor que o valor"+ c);
+}
